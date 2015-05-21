8b8cd624addc726f3971389246682495c7c26e50
Alexey Alekhin <laughedelic@gmail.com>
2015-05-21 12:24:36 +0200

    Upgraded to reveal.js 3.0

--- src/main/g8/compile.revealjs.sh
+++ src/main/g8/compile.revealjs.sh
@@ -2,6 +2,6 @@
 pandoc -t revealjs --template=template.revealjs \
     --standalone --smart \
     --slide-level=2 \
-    --variable theme="moon" \
-    --variable transition="linear" \
+    --variable theme="white" \
+    --no-highlight \
     $1 -o ${1%.md}.html "${@:2}"
